a635e79860c9819a6e7ef9056380237a3aef27b8
domitalk <dominic.m.chu@gmail.com>
2020-05-27 14:18:54 -0400

    background

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-        1. Blank space - parallax background with the blank showing through 
-        2. Flat wide card fit to screen w/ photo of self + image links to everything 
-        3. Projects section 
-          a. wide or double col
-        4. bottom of page links also 
-        5. floating nob that stays at the bottom right that takes you back to the top
-
-        // maybe add css animation w/parallax so that title for secions show up as you scroll
-        // even play around with white space that's blocking out the background and
-        // change tone as you scroll down 
+    <div className="container">
+      <div>navigation bar</div>
+      <div className="blank-space">.</div>
+      <div>info</div>
+      <div>projects container</div>
     </div>
   );
 }
 
 export default App;
+
+// 1. Blank space - parallax background with the blank showing through 
+// 2. Flat wide card fit to screen w/ photo of self + image links to everything 
+// 3. Projects section 
+//   a. wide or double col
+// 4. bottom of page links also 
+// 5. floating nob that stays at the bottom right that takes you back to the top
+
+// // maybe add css animation w/parallax so that title for secions show up as you scroll
+// // even play around with white space that's blocking out the background and
+// // change tone as you scroll down 
